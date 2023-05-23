4e0717c42900f464c7bf9cd6704a0158b9b56bec
behdad jahad <behdadja@gmail.com>
2023-05-23 16:46:33 +0330

    tree data structure in python

---
+++ tree.py
@@ -0,0 +1,11 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def remove_child(self, child):
+        self.children.remove(child)
+
